11ea22c7ea7de1cd2befd69b3d845fa3fa405923
Kyle Repinski <repinski23@gmail.com>
2015-04-27 15:36:58 -0500

    libc: Don't build malloc leak check for userdebug.
    Change-Id: I067fed514cf427d4b4de8c29586f3452b5af2aec

--- libc/Android.mk
+++ libc/Android.mk
@@ -1452,7 +1452,7 @@ LOCAL_ALLOW_UNDEFINED_SYMBOLS := true
 LOCAL_LDFLAGS := -Wl,--version-script,$(LOCAL_PATH)/version_script.txt
 
 # Don't install on release build
-LOCAL_MODULE_TAGS := eng debug
+LOCAL_MODULE_TAGS := eng
 LOCAL_ADDRESS_SANITIZER := false
 LOCAL_NATIVE_COVERAGE := $(bionic_coverage)
 
@@ -1492,7 +1492,7 @@ LOCAL_SYSTEM_SHARED_LIBRARIES :=
 LOCAL_LDFLAGS := -Wl,--version-script,$(LOCAL_PATH)/version_script.txt
 
 # Don't install on release build
-LOCAL_MODULE_TAGS := eng debug
+LOCAL_MODULE_TAGS := eng
 LOCAL_ADDRESS_SANITIZER := false
 LOCAL_NATIVE_COVERAGE := $(bionic_coverage)
 
